4221a8422839691fc96875c14ecea24e0a4eb078
Merdi Elongo <merdielongo9@gmail.com>
2024-01-22 12:49:10 +0100

    initialisation

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,18 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { sassPlugin } from "vite-plugin-sass";
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), sassPlugin()],
-  build: {
-    lib: {
-      entry: "src/index.ts",
-      name: "SchoolapUI",
-      fileName: (format) => `schoolap-ui.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["vue"],
-      output: {
-        globals: {
-          vue: "Vue",
-        },
-      },
-    },
-  },
-});
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
