7124144cbfe7ce0bd9492b8031129ee4974ebd63
merdilosima <99757865+merdilosima@users.noreply.github.com>
2024-01-24 14:51:11 +0100

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import SPButtonPrimary from './src/components/buttons/SPButtonPrimary.vue'
+import SPButtonGoogle from './src/components/buttons/SPButtonGoogle.vue'
+
+export { SPButtonPrimary, SPButtonGoogle }
