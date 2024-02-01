8c88a0a53d2aa39b89a311bc1666ccb91a941d03
michaelschoolap <michael.mogolo@schoolap.org>
2024-02-01 14:01:46 +0100

    feat: :white_check_mark: Add card_pricing component and import it in necessary files

--- src/SchoolapUI.ts
+++ src/SchoolapUI.ts
@@ -19,6 +19,8 @@ import SPBreadCrump from './components/breadcrump/SPBreadcrump.vue'
 import SPCardActivity from './components/cards/SPCardActivity.vue'
 import SPStat from './components/cards/SPStat.vue'
 import SPSideBar from './components/navigation/sidebar/SPSideBar.vue'
+import SPCardProfilSchool from './components/cards/SPCardProfilSchool.vue'
+import SPCardPricing from './components/cards/SPCardPricing.vue'
 
 import './assets/scss/main.scss'
 
@@ -46,6 +48,8 @@ const SchoolapUI = {
     app.component('sp-stat', SPStat)
     app.component('sp-card-activity', SPCardActivity)
     app.component('sp-side-bar', SPSideBar)
+    app.component('sp-card-profil-school', SPCardProfilSchool)
+    app.component('sp-card-pricing', SPCardPricing)
   }
 }
 
