63e120fcef82f0fff14529fef4c7110ba4e071dc
michaelschoolap <michael.mogolo@schoolap.org>
2024-02-02 10:31:29 +0100

    feat: :white_check_mark: Add card_other_action component

--- src/components/buttons/SPButtonIcon.vue
+++ src/components/buttons/SPButtonIcon.vue
@@ -4,12 +4,7 @@
             <div class="title">{{ title }}</div>
             <div class="svg-right">
                 <img class="svg-img" :src="src">
-                <!-- <svg width="20" height="22" viewBox="0 0 20 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12.025 5.81915L17.0834 11.1304L12.025 16.4417" stroke="white" stroke-width="1.5"
-                        stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M2.91663 11.1304L16.9416 11.1304" stroke="white" stroke-width="1.5" stroke-miterlimit="10"
-                        stroke-linecap="round" stroke-linejoin="round" />
-                </svg> -->
+                
             </div>
         </template>
         <template v-else>
