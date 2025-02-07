e946f57604797abf50f96db4fcc82b8915dac6e3
glorianeSchoolap <gloriane.kingolo@schoolap.org>
2025-02-07 13:17:44 +0100

    retrait text-decoration-none

--- src/components/cards/SPStat.vue
+++ src/components/cards/SPStat.vue
@@ -1,54 +1,57 @@
 <template>
-  <div class="sp-stat">
-    <router-link :to="{ name: route }" class="bloc_one cursor-pointer">
-      <h2>{{ title }}</h2>
-      <div class="bloc_svg">
-        <img :src="svg" />
-        <span class="counter">{{ counter }}</span>
-      </div>
-      <template v-if="showOnlyDescription">
-        <h3 class="sp_add">{{ description }}</h3>
-      </template>
-      <template v-else>
-        <ul class="sp-stat-list">
-          <li>{{ number_men }}</li>
-          <li>{{ number_women }}</li>
-        </ul>
-      </template>
-    </router-link>
-    <div class="bloc_two cursor-pointer" @click="$emit('add')">
-      <img :src="img_button_action" />
+    <div class="sp-stat">
+        <router-link :to="route" class="bloc_one cursor-pointer text-decoration-none">
+            <h2>{{ title }}</h2>
+            <div class="bloc_svg">
+                <img :src="svg">
+                <span class="counter">{{ counter }}</span>
+            </div>
+            <template v-if="showOnlyDescription">
+                <h3 class="sp_add">{{ description }}</h3>
+            </template>
+            <template v-else>
+                <ul class="sp-stat-list">
+                    <li>{{ number_men }}</li>
+                    <li> {{ number_women }}</li>
+                </ul>
+            </template>
+
+        </router-link >
+        <div class="bloc_two cursor-pointer" @click="$emit('add')">
+            <img :src="img_button_action">
+        </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
+
 export default {
-  props: {
-    svg: String,
-    title: String,
-    img_button_action: String,
-    description: String,
-    counter: {
-      default: 0,
-      type: Number
-    },
-    showOnlyDescription: {
-      type: Boolean,
-      default: true
-    },
-    number_men: {
-      default: '',
-      type: String
-    },
-    number_women: {
-      default: '',
-      type: String
-    },
-    route: {
-      default: '',
-      type: String,
+
+    props: {
+        svg: String,
+        title: String,
+        img_button_action: String,
+        description: String,
+        counter: {
+            default: 0,
+            type: Number
+        },
+        showOnlyDescription: {
+            type: Boolean,
+            default: true
+        },
+        number_men: {
+            default: '',
+            type: String
+        },
+        number_women: {
+            default: '',
+            type: String
+        }, 
+        route:{
+            default: '',
+            type: String
+        }
     }
-  }
 }
 </script>
